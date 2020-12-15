041f0895c6bac85a87aa4a31537c5472fe858f24
Diana Che <dianache@Dianas-MBP.attlocal.net>
2020-12-15 09:50:35 -0600

    Highlighted space debris (pt.6 A6)

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,6 +4,7 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
+
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
         this.name = name;
         this.orbitType = orbitType;
@@ -11,5 +12,10 @@ export class Satellite {
         this.operational = operational;
         this.launchDate = launchDate;
     }
-}
 
+    shouldShowWarning(): boolean {
+        if(this.type === 'Space Debris') {
+           return true;
+        } 
+  }
+}
